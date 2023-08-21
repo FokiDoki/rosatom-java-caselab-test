92b0f53a488582a9cc74c2ca68a66e99413f21b2
FokiDoki <23121394+FokiDoki@users.noreply.github.com>
2023-08-21 17:15:43 +0300

    Update task-2-swap.java

--- task-2-swap.java
+++ task-2-swap.java
@@ -1,14 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-        swap(5, 10);
-        swap(10, 5);
-        swap(0, 1);
-        swap(1, 0);
-        swap(1, 1);
-        swap(-1, 1);
-        swap(-10, -15);
-        swap(10, -15);
+        swap(5, 10);   // a = 10, b = 5
+        swap(10, 5);   // a = 5, b = 10
+        swap(0, 1);    // a = 1, b = 0
+        swap(1, 0);    // a = 0, b = 1
+        swap(1, 1);    // a = 1, b = 1
+        swap(-1, 1);   // a = 1, b = -1
+        swap(-10, -15);// a = -15, b = -10
+        swap(10, -15); // a = -15, b = 10
     }
 
     public static void swap(int a, int b){
